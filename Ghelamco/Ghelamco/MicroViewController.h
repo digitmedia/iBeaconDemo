0d487dfb40ed0695a07a623c6ff81e0effefa0ef
Spencer Pieters <spencer.pieters@icapps.be>
2013-10-24 11:30:18 +0200

    Tijdelijk hoofdscherm om te switchen tussen micro en macro

---
+++ Ghelamco/Ghelamco/MicroViewController.h
@@ -0,0 +1,15 @@
+//
+//  MicroViewController.h
+//  Ghelamco
+//
+//  Created by Spencer Pieters on 24/10/13.
+//  Copyright (c) 2013 Spencer Pieters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MicroViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *seatView;
+
+@end
---
+++ Ghelamco/Ghelamco/MicroViewController.m
@@ -0,0 +1,29 @@
+//
+//  MicroViewController.m
+//  Ghelamco
+//
+//  Created by Spencer Pieters on 24/10/13.
+//  Copyright (c) 2013 Spencer Pieters. All rights reserved.
+//
+
+#import "MicroViewController.h"
+
+@interface MicroViewController ()
+
+@end
+
+@implementation MicroViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.title = @"Venue";
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
